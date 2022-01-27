c58ef552be1c4f5b8a53451aa9ef51f2c82899be
mak-1920 <roma.ivanov.1920@mail.ru>
2022-01-27 15:18:42 +0300

    bug fix

--- src/DoctrineWriter.php
+++ src/DoctrineWriter.php
@@ -6,8 +6,8 @@ use Port\Doctrine\Exception\UnsupportedDatabaseTypeException;
 use Port\Writer;
 use Doctrine\Common\Util\Inflector;
 use Doctrine\DBAL\Logging\SQLLogger;
-use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\Common\Persistence\Mapping\ClassMetadata;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Persistence\Mapping\ClassMetadata;
 
 /**
  * A bulk Doctrine writer
